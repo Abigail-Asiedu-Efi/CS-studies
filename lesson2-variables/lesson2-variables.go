dd306e341dfa366d362d1a12766dd5a51046dfe7
Abigail-Asiedu-Efi <daddypearl12032003@gmail.com>
2025-05-01 15:07:08 +0000

    Add lesson2 file for variables in Go

---
+++ lesson2-variables/lesson2-variables.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a string = "Programming"
+	fmt.Println(a)
+
+	var b, c, d int = 2, 7, 6
+	fmt.Println(b, c, d)
+
+	e := false
+	fmt.Println(e)
+
+	var f int
+	fmt.Println(f)
+
+	var m int = 24
+	var n float64 = 2.56
+	fmt.Println("Integer: ", m)
+	fmt.Println("Float: ", n)
+
+}
